2857c538c877e9d56c0395c74e10cce87d0165c4
dan14 <danielayala1793@hotmail.com>
2022-08-14 17:57:07 -0500

    Changes 14-8-2022

--- app/Http/Controllers/HolaController.php
+++ app/Http/Controllers/HolaController.php
@@ -9,9 +9,25 @@ class HolaController extends Controller{
     }
 
     public function db(int $id){
-        $users = DB::table('fortalezas')->get("id")->where("id",$id);
+        //$users = DB::table('fortalezas')->get("id")->where("id",$id);//1ra forma de consultar
+        //$users=DB::select("select elemento from fortalezas where id={$id}");//2da forma de consultar
+        /*$tablas=DB::select("SELECT * FROM pg_catalog.pg_tables WHERE schemaname !=
+        'pg_catalog' AND schemaname != 'information_schema'");
+        //return json_encode($tablas);
+        /*$arrayTables=array();
+        $long=sizeOf($tablas);
+        for ($i=0; $i < $long; $i++) {
+            array_push($arrayTables,$tablas[$i]->tablename);
+        }
+        return $arrayTables;*/
+        //$tablas=DB::select("SELECT table_name FROM information_schema.tables");//info de todas las tablas de postgres
 
-        return $users;
+        //consulta de las tablas que hay en mi database
+        $tablas=DB::select("SELECT table_name --seleccionamos solo la columna del nombre de la tabla
+        FROM information_schema.tables --seleccionamos la información del esquema
+        WHERE table_schema='public' --las tablas se encuentran en el esquema publico
+        AND table_type='BASE TABLE'; --tiene que ser del tipo table ya que aqui se listan tambien las vistas");
+        return $tablas;
     }
 
 }
